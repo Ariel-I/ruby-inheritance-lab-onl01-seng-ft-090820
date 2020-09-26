de7327be2667eb0dedc5720dbc74bd1e28aa5d07
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-26 06:41:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
 require_relative "./user.rb"
 
 class Student < User
+  
+  attr_accessor :knowledge
+  
+  def initialize(knowledge)
+    @knowledge = []
+  end
+
+  def learn 
+  end
+
+  def knowledge
+    return @knowledge
+  end
 
 end
